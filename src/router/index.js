94738bb36ddfb2df620b3df031bb115751a27b23
Ridwansyah-glitch <ridwansyah00693@gmail.com>
2020-07-22 13:53:58 +0700

    Upload projek vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Product from "../views/Product.vue";
+import ShoppingCart from "../views/ShoppingCart.vue";
+import Success from "../views/Success.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +21,21 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/product",
+    name: "Product",
+    component: Product
+  },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: ShoppingCart
+  },
+  {
+    path: "/Success",
+    name: "Success",
+    component: Success
   }
 ];
 
